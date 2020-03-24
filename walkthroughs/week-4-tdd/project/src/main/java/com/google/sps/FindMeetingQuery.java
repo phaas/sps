a28b6666d9edee7b1c99072e83a2a7b58a165097
Patrick Haas <patrick@jphaas.net>
2020-03-24 14:46:57 -0700

    Implement Meeting Query service.

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,72 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+	public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+		List<TimeRange> relevantEvents = new ArrayList<>();
+		for (Event event : events) {
+			for (String attendee : event.getAttendees()) {
+				if (request.getAttendees().contains(attendee)) {
+					relevantEvents.add(event.getWhen());
+					break;
+				}
+			}
+		}
+
+		List<TimeRange> openTimes = findAvailableTimes(relevantEvents);
+		List<TimeRange> suitableTimes = openTimes.stream()
+				.filter(time -> time.duration() >= request.getDuration())
+				.collect(Collectors.toList());
+		return suitableTimes;
+	}
+
+	private List<TimeRange> findAvailableTimes(List<TimeRange> conflicts) {
+		conflicts.sort(TimeRange.ORDER_BY_START);
+		List<TimeRange> options = new ArrayList<>();
+
+		int now = TimeRange.START_OF_DAY;
+		for (TimeRange when : conflicts) {
+			if (now < when.start()) {
+				options.add(TimeRange.fromStartEnd(now, when.start(), false));
+			}
+			if (now < when.end()) {
+				now = when.end();
+			}
+		}
+		if (now < TimeRange.END_OF_DAY) {
+			options.add(TimeRange.fromStartEnd(now, TimeRange.END_OF_DAY, true));
+		}
+		return options;
+	}
+
+
+	public Collection<TimeRange> queryGolf(Collection<Event> events, MeetingRequest request) {
+		List<TimeRange> unavailableTimes = events.stream()
+				.filter(e -> !Collections.disjoint(e.getAttendees(), request.getAttendees()))
+				.map(Event::getWhen)
+				.sorted(TimeRange.ORDER_BY_START)
+				.collect(Collectors.toList());
+
+		List<TimeRange> options = new ArrayList<>();
+		int now = TimeRange.START_OF_DAY;
+		for (TimeRange when : unavailableTimes) {
+			if (now < when.start()) {
+				options.add(TimeRange.fromStartEnd(now, when.start(), false));
+			}
+			now = Math.max(now, when.end());
+		}
+		if (now < TimeRange.END_OF_DAY) {
+			options.add(TimeRange.fromStartEnd(now, TimeRange.END_OF_DAY, true));
+		}
+		return options.stream()
+				.filter(time -> time.duration() >= request.getDuration())
+				.collect(Collectors.toList());
+	}
+
 }
